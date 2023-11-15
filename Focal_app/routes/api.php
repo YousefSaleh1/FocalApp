70217060a94d9a332cbeefbdc8549dc14e2a3315
IT <ys9882111@gmail.com>
2023-11-15 16:25:53 +0200

    review Controller And testing

--- Focal_app/routes/api.php
+++ Focal_app/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
